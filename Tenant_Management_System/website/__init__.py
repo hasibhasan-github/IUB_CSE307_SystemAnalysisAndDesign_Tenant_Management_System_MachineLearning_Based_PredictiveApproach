a86d2b74d3ce01e5af4c68217f0a3e789f48001a
hasibhasan-github <hasibhasan483@gmail.com>
2025-05-01 02:18:47 +0600

    @hasibhasan-github

--- Tenant_Management_System/website/__init__.py
+++ Tenant_Management_System/website/__init__.py
@@ -19,13 +19,6 @@ def create_app():
 
     db.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int())
 
     from .views import views
     from .auth import auth
@@ -38,6 +31,15 @@ def create_app():
     )
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 def create_database(app):
