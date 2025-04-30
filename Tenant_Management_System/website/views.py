90c285c8fbb4ab724e301f15f50543d5c19501e6
hasibhasan-github <hasibhasan483@gmail.com>
2025-05-01 02:23:07 +0600

    @hasibhasan-github

--- Tenant_Management_System/website/views.py
+++ Tenant_Management_System/website/views.py
@@ -20,36 +20,36 @@ def explore():
 @views.route('/profile')
 @login_required
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", user = current_user)
 
 @views.route('/info')
 @login_required
 def info():
-    return render_template("info.html")
+    return render_template("info.html", user = current_user)
 
 @views.route('/report')
 @login_required
 def report():
-    return render_template("report.html")
+    return render_template("report.html", user = current_user)
 
 @views.route('/noti')
 @login_required
 def noti():
-    return render_template("noti.html")
+    return render_template("noti.html", user = current_user)
 
 @views.route('/service')
 @login_required
 def service():
-    return render_template("service.html")
+    return render_template("service.html", user = current_user)
 
 @views.route('/pay')
 @login_required
 def pay():
-    return render_template("pay.html")
+    return render_template("pay.html", user = current_user)
 
 @views.route('/pred')
 @login_required
 def pred():
-    return render_template("pred.html")
+    return render_template("pred.html", user = current_user)
 
 # Profile Templates Route
