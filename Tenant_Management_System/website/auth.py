77e24830edc9bf8ad72ec1db8ddd1efc6170290f
hasibhasan-github <hasibhasan483@gmail.com>
2025-04-30 18:20:20 +0600

    @hasibhasan-github

--- Tenant_Management_System/website/auth.py
+++ Tenant_Management_System/website/auth.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -11,6 +11,6 @@ def login():
 def logout():
     return 
 
-@auth.route('/signup')
+@auth.route('/signup',  methods = ['GET', 'POST'])
 def signup():
     return render_template("signup.html")
