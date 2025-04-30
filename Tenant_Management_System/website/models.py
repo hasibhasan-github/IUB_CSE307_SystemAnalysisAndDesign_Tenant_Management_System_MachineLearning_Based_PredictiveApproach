75a264e00b6aad692cb8a8b61b9590f7d25743a8
hasibhasan-github <hasibhasan483@gmail.com>
2025-04-30 21:08:35 +0600

    @hasibhasan-github

--- Tenant_Management_System/website/models.py
+++ Tenant_Management_System/website/models.py
@@ -2,4 +2,6 @@ from . import db
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    id = db.Coloumn(db.Integer, primary_key = true)
+    id = db.Coloumn(db.Integer, primary_key = True)
+    email = db.Coloumn(db.String(30), unique = True)
+    password = db.Coloumn(db.String(30))
