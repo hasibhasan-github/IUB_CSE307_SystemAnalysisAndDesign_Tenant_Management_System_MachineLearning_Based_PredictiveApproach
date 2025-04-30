60087308f4025b74a088cffdcc42505e4b335446
hasibhasan-github <hasibhasan483@gmail.com>
2025-04-30 23:25:13 +0600

    @hasibhasan-github

--- Tenant_Management_System/website/__init__.py
+++ Tenant_Management_System/website/__init__.py
@@ -9,22 +9,25 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'DHFGHJASDVFHGFHVCDCDJU153'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///path/to/database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///website/{DB_NAME}'
     db.init_app(app)
 
     from .views import views
     from .auth import auth
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Verification, LeaseAgreement, MaintenanceRequest, Rent, Property, ServiceProvider, ServiceBill, Notification
+    from .models import (
+        User, Verification, LeaseAgreement, MaintenanceRequest,
+        Rent, Property, ServiceProvider, ServiceBill, Notification
+    )
 
-    create_database()
-
-    app.register_blueprint(views, url_prefix = '/')
-    app.register_blueprint(auth, url_prefix = '/')
-
+    create_database(app)
     return app
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print("created")
+        with app.app_context():
+            db.create_all()
+            print("Database created")
+
