9a893278fd491ee0f35efd9368606f048b004526
hasibhasan-github <hasibhasan483@gmail.com>
2025-04-30 16:09:03 +0600

    @hasibhasan-github

--- Tenant_Management_System/website/views.py
+++ Tenant_Management_System/website/views.py
@@ -14,6 +14,12 @@ def homeg():
 def explore():
     return render_template("base.html")
 
+# Profile Templates Route
+
 @views.route('/profile')
 def profile():
     return render_template("profile.html")
+
+@views.route('/info')
+def info():
+    return render_template("info.html")
