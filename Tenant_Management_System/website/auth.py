8079b499469ffaafa1962661ed7fe110cdcaddae
hasibhasan-github <hasibhasan483@gmail.com>
2025-05-01 16:25:56 +0600

    @hasibhasan-github

--- Tenant_Management_System/website/auth.py
+++ Tenant_Management_System/website/auth.py
@@ -75,7 +75,7 @@ def signup():
             db.session.add(new_user)
             db.session.commit()
             flash('Registration Successful', category='success')
-            login_user(user, remember=True)
+            # login_user(user, remember=True)
             return redirect(url_for('views.home'))
 
     return render_template("signup.html")
